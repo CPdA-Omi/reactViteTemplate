f34c7b2cfd95edc834fd682332652f52a5a16805
CPdA-Omi <charlie.perrondarc@gmail.com>
2024-04-02 17:29:09 +0200

    feat: Add props types for PokemonCard

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import PokemonCard from './components/PokemonCard.jsx'
 const pokemonList = [
   {name: "bulbasaur", imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" },
-  {name: "mew"},
+  {name: "mew"}
 ];
 
 function App() {

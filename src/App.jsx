de83a7f050456c5eb054eb357d95240b1a9d456a
CPdA-Omi <charlie.perrondarc@gmail.com>
2024-04-02 12:02:38 +0200

    fix: Remove the count setter

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,7 @@ import PokemonCard from './components/PokemonCard.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <div>
       <PokemonCard/>

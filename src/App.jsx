c3a857ec54dc705bdc774321f224f5d30ac4782e
CPdA-Omi <charlie.perrondarc@gmail.com>
2024-04-02 16:23:04 +0200

    feat: Pokemon Card now works with an array

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 import PokemonCard from './components/PokemonCard.jsx'
 
 import './App.css'
 
 function App() {
-  
+
   return (
-    <div>
+    <>
       <PokemonCard/>
-    </div>
+    </>
   )
 }
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,20 @@
-export default function PokemonCard(){
+const pokemonList = [
+	{name: "bulbasaur", imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" },
+	{name: "mew"},
+];
+
+function PokemonCard(){
+	
+	const pokemon = pokemonList[0];
+
 	return(
-		<figure>
-			<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur"/>
-			<figcaption>Bulbasaur</figcaption>
-		</figure>
+		<>
+			<figure>
+				{pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name}/> : <p title="No image available for this pokemon">???</p>}
+				<figcaption>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</figcaption>
+			</figure>
+		</>
 	);
 }
+
+export default PokemonCard;

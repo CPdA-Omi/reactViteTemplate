c781af52e89e52686d7c0b330f74e6f85b6230d3
CPdA-Omi <charlie.perrondarc@gmail.com>
2024-04-03 10:16:36 +0200

    feat: Add primary navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,50 @@ import { useState } from 'react'
 import './App.css'
 
 import PokemonCard from './components/PokemonCard.jsx'
+const imgURLTemplate = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
 const pokemonList = [
-  {name: "bulbasaur", imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" },
-  {name: "mew"}
+  {name: 'bulbasaur', imgSrc: imgURLTemplate + 1 + '.png'},
+  {name: 'ivysaur', imgSrc: imgURLTemplate + 2 + '.png'},
+  {name: 'venusaur', imgSrc: imgURLTemplate + 3 + '.png'},
+  {name: 'charmender', imgSrc: imgURLTemplate + 4 + '.png'},
+  {name: 'charmeleon', imgSrc: imgURLTemplate + 5 + '.png'},
+  {name: 'charizard', imgSrc: imgURLTemplate + 6 + '.png'},
+  {name: 'squirtle', imgSrc: imgURLTemplate + 7 + '.png'},
+  {name: 'wartortle', imgSrc: imgURLTemplate + 8 + '.png'},
+  {name: 'blastoise', imgSrc: imgURLTemplate + 9 + '.png'},
+  {name: 'mew'},
 ];
 
 function App() {
 
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handleClickDecrement = () => {
+    if (pokemonIndex > 0){
+      setPokemonIndex(pokemonIndex - 1);
+    }
+  }
+
+  const handleClickIncrement = () => {
+    if (pokemonIndex < pokemonList.length - 1){
+      setPokemonIndex(pokemonIndex + 1);
+    }
+  }
+
   return (
     <>
-      <PokemonCard pokemon={pokemonList[0]}/>
+      <div>
+        {pokemonIndex > 0 ?
+        <button onClick={handleClickDecrement}>Previous</button>
+        :
+        <button disabled>Previous</button>}
+        
+        {pokemonIndex < pokemonList.length -1 ?
+        <button onClick={handleClickIncrement}>Next</button>
+        :
+        <button disabled>Next</button>}
+      </div>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
     </>
   )
 }
